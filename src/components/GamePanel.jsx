4f2edc6ce479c3e6d6427fa735715973d375ddaa
Kris Hvattum <klhvattum@live.com>
2023-04-20 22:00:46 -0700

    Displays 3 random countries plus the correct one on buttons. currentCountry and button names stored as state

--- src/components/GamePanel.jsx
+++ src/components/GamePanel.jsx
@@ -1,42 +1,87 @@
 import { Button } from 'reactstrap';
+import { useState, useEffect } from 'react';
+import axios from 'axios'
 
 const GamePanel = () => {
-  return (
-    <div>
-        <div className='countryDisplayDiv'>
-            <h3 className='countryName'>Which country am I??</h3>            
-        </div>
+    const [currentCountry, setCurrentCountry] = useState(null);
+    const [buttonNames, setButtonNames] = useState([]);
 
-        <div className='countryInfoDiv'>
-            <ul className='countryInfoList'>
-                <li className='infoItem population'>
-                    <p>
-                        Population: 
-                        <span id='population'> 1,300,000</span>
-                    </p>
-                </li>
-                <li className='infoItem area'>
-                    <p>
-                        Area: 
-                        <span id='area'> 5,000 square miles</span>
-                    </p>
-                </li>                
-                <li className='infoItem'>
-                    <p className='flag' id='flag'>
-                        🇲🇽
-                    </p>
-                </li>                
-            </ul>
-        </div>
+    useEffect(() => {
+        const getRandomCountry = async () => {
 
-        <div className='buttonDiv'>
-            <Button className='mx-2'>Japan</Button>
-            <Button className='mx-2'>Mexico</Button>
-            <Button className='mx-2'>South Africa</Button>
-            <Button className='mx-2'>Turkey</Button>
-        </div>
-    </div>
-  )
+            const response = await axios.get('https://restcountries.com/v2/all');
+            const countries = response.data;
+            const randomIndex = Math.floor(Math.random() * countries.length);
+            const randomCountry = countries[randomIndex];
+            setCurrentCountry(randomCountry);
+
+            const names = [randomCountry.name]
+            while (names.length < 4) {
+                const randomName = countries[Math.floor(Math.random() * countries.length)].name;
+                if (!names.includes(randomName)) {
+                    names.push(randomName);
+                }
+            }
+ 
+            const shuffledNames = names.sort(() => Math.random() - 0.5);
+            setButtonNames(shuffledNames);
+            console.log(buttonNames)
+        };
+
+        getRandomCountry(); 
+        console.log(currentCountry);
+
+    }, []); 
+
+
+    return (
+        <>
+        { currentCountry ? (
+        <div>
+            <div className='countryDisplayDiv'>
+                <h3 className='countryDisplay' id='countryDisplay'>Which country am I?? { currentCountry.name }</h3>
+            </div>
+
+            <div className='countryInfoDiv'>
+                <ul className='countryInfoList'>
+                    <li className='infoItem population'>
+                        <p>
+                            Population:
+                            <span id='population'> { currentCountry.population }</span>
+                        </p>
+                    </li>
+                    <li className='infoItem area'>
+                        <p>
+                            Area:
+                            <span id='area'> { currentCountry.area} </span>
+                            square miles
+                        </p>
+                    </li>
+                    <li className='infoItem region'>
+                        <p>
+                            Region:
+                            <span id='region'> { currentCountry.region} </span>
+                        </p>
+                    </li>
+                    <li className='infoItem'>
+                        <img className='flag' id='flag' src={currentCountry.flag} style={{width: '200px'}} />
+                    </li>
+                </ul>
+            </div>
+
+            <div className='buttonDiv'>
+                <Button className='mx-2'>{buttonNames[0]}</Button>
+                <Button className='mx-2'>{buttonNames[1]}</Button>
+                <Button className='mx-2'>{buttonNames[2]}</Button>
+                <Button className='mx-2'>{buttonNames[3]}</Button>
+            </div>
+        </div >            
+        ) : 
+            <p>Loading country data</p>
+    }        
+        </>
+        
+    )
 }
 
-export default GamePanel
+export default GamePanel;
