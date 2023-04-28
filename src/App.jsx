b7225c9d4348dcb88b72e4c195131d5090969d60
Kris Hvattum <klhvattum@live.com>
2023-04-27 21:30:23 -0700

    Position pageTitleSmall on left

--- src/App.jsx
+++ src/App.jsx
@@ -2,15 +2,14 @@ import { useState } from 'react'
 import './App.css';
 import GamePanel from './components/gamePanel'; 
 import Footer from './components/Footer';
+import Title from './components/Title';
 
 function App() {
 
   return (
     <div className='app'>
-        <p className='pageTitleSmall' id='pageTitleSmall'>Country Guesser</p>
-        <h1 className='pageTitle' id='pageTitle'>Country Guesser!</h1>
-        <h2 className='pageSubtitle' id='pageSubtitle'>Guess the country based on the info provided. How long can you keep your streak alive?</h2>
 
+        <Title />
         <GamePanel />
         <Footer />
     </div>
