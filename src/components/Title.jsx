90678dab17643a42e73d6072ce9fdbdbb63f955e
Kris Hvattum <klhvattum@live.com>
2023-05-02 14:51:12 -0700

    Add high score messageto pageTitleSmall

--- src/components/Title.jsx
+++ src/components/Title.jsx
@@ -1,7 +1,7 @@
 const Title = () => {
     return (
         <div className="text-center">
-            <p className='pageTitleSmall' id='pageTitleSmall'>Country Guesser</p>
+            <p className='pageTitleSmall' id='pageTitleSmall'>Country Guesser (current high score: 193 [RP])</p>
             <h1 className='pageTitle' id='pageTitle'>Country Guesser!</h1>
             <h2 className='pageSubtitle' id='pageSubtitle'>Guess the country based on the info provided. How long can you keep your streak alive?</h2></div>
     )
