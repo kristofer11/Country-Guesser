1ea028871e28b9d878dc03c1f9d618718b947e3d
Kris Hvattum <klhvattum@live.com>
2023-05-18 20:00:47 -0700

    Remove part of footer message and header message regarding api being taken-down

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,8 +1,7 @@
 const Footer = () => {
   return (
     <footer className="footer mt-auto">
-        <p>Special thanks to the middle schoolers at MCS for the continued assistance in testing this project. Your brutal honesty hurt at times, but in the end led to a better product.</p>
-        <p>At your request, region has been added to the hint section. Enjoy!</p>
+        <p>Special thanks to the middle schoolers at MCS for your assistance in testing this project.</p>
         <p>&copy; 2023 K. Hvattum</p>
     </footer>
   )
--- src/components/Title.jsx
+++ src/components/Title.jsx
@@ -2,7 +2,6 @@ const Title = () => {
     return (
         <div className="text-center">
             <p className='pageTitleSmall' id='pageTitleSmall'>Country Guesser</p>
-            <p>It appears that the API used to obtain the data on this site will be taken down sometime in May, 2023. Please enjoy the game while you can!</p>
             <h1 className='pageTitle' id='pageTitle'>Country Guesser!</h1>
             <h2 className='pageSubtitle' id='pageSubtitle'>Guess the country based on the info provided. How long can you keep your streak alive?</h2></div>
     )
