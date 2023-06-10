0cf3010854c470fe0ef35844ddd8f2a7e589e10f
Kris Hvattum <klhvattum@live.com>
2023-06-10 13:01:57 -0700

    Implement top 10 leaders only displayed (Leaders.jsx)

--- src/components/Leaders.jsx
+++ src/components/Leaders.jsx
@@ -3,37 +3,42 @@ import axios from 'axios';
 import useLeadersData from '../features/useLeadersData';
 
 const Leaders = () => {
-  const leadersData = useLeadersData();
+    const leadersData = useLeadersData();
 
-//   useEffect(() => {
-//     const fetchData = async () => {
-//       try {
-//         const response = await axios.get('https://secret-mountain-38731.herokuapp.com/api/leaders');
-//         setLeadersData(response.data);
-//       } catch (error) {
-//         console.error(error);
-//       }
-//     };
+    const sortedLeadersData = leadersData
+        ? [...leadersData].sort((a, b) => b.streak - a.streak) : [];
 
-//     fetchData();
-//   }, []);
+    const top10Leaders = sortedLeadersData.slice(0, 10);
 
-  return (
-    <div className='leaderBoard'>
-    <h2 style={{marginTop: '4rem'}}>Leader Board</h2>
-      
-      {leadersData ? (
-        <ul className='leaderList' style={{marginBottom: '4rem'}}>
-          {leadersData.map((leader) => (
-            <li key={leader.id}>{leader.name}: {leader.streak} <span style={{color: 'green'}}>||</span></li>
-            
-          ))}
-        </ul>
-      ) : (
-        <p>Loading...</p>
-      )}
-    </div>
-  );
+    //   useEffect(() => {
+    //     const fetchData = async () => {
+    //       try {
+    //         const response = await axios.get('https://secret-mountain-38731.herokuapp.com/api/leaders');
+    //         setLeadersData(response.data);
+    //       } catch (error) {
+    //         console.error(error);
+    //       }
+    //     };
+
+    //     fetchData();
+    //   }, []);
+
+    return (
+        <div className='leaderBoard'>
+            <h2 style={{ marginTop: '4rem' }}>Leader Board</h2>
+
+            {top10Leaders.length > 0 ? (
+                <ul className='leaderList' style={{ marginBottom: '4rem' }}>
+                    {top10Leaders.map((leader) => (
+                        <li key={leader.id}>{leader.name}: {leader.streak} <span style={{ color: 'green' }}>||</span></li>
+
+                    ))}
+                </ul>
+            ) : (
+                <p>Loading...</p>
+            )}
+        </div>
+    );
 };
 
 export default Leaders;
