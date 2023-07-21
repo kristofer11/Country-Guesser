60c6a17cc1591f8d90e50a72a9943a2771011a45
Kris Hvattum <klhvattum@live.com>
2023-07-20 22:09:34 -0700

    remove message from footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,7 +1,6 @@
 const Footer = () => {
   return (
     <footer className="footer mt-auto">
-        <p>Special thanks to the middle schoolers at MCS for your assistance in testing this project.</p>
         <p>&copy; 2023 K. Hvattum</p>
     </footer>
   )
